7299404ae3d5602f248a7ff0606dd9481c355665
Mateusz Grzegorzek <gnegon2@gmail.com>
2015-01-16 22:03:06 +0100

    W jednym okienku wysyłanie i odbieranie po RS. W drugim okienku wysyłanie i odbieranie po TCP.

---
+++ EtherCOM/EtherCOM/MainForm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace EtherCOM
+{
+    public partial class EtherCOM : Form
+    {
+        public EtherCOM()
+        {
+            InitializeComponent();
+        }
+
+        private void EtherCOM_OnLoad(object sender, EventArgs e)
+        {               
+            // Initializace COM Ports
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+                Com.Items.Add(port);
+            //Default parameters
+            ModuleIP.Text = "192.168.2.102";
+            Port.Text = "7";
+
+            Com.Text = "COM8";
+            Baudrate.Text = "115200";
+            Databits.Text = "8";
+            Parity.SelectedIndex = 0;
+        }
+
+        private void EtherCOM_Connect(object sender, EventArgs e)
+        {
+            if (ModuleIP.Text.Length > 0 && Port.Text.Length > 0)
+            {
+                string portName = Com.Text;
+                int baudRate = Convert.ToInt32(Baudrate.Text);
+                Parity parity = System.IO.Ports.Parity.None;
+                int dataBits = Convert.ToInt32(Databits.Text);
+                StopBits stopBits = StopBits.One;
+                SerialPort serial_port = new SerialPort(portName, baudRate, parity, dataBits, stopBits);
+
+                RsForm RsFormInstance = new RsForm();
+                RsFormInstance.Show();
+                RsFormInstance.Location = new Point(this.Location.X + this.Width, this.Location.Y);
+                RsFormInstance.Init(serial_port);
+
+                TcpForm TcpFormInstance = new TcpForm();
+                TcpFormInstance.Show();
+                TcpFormInstance.Location = new Point(this.Location.X + this.Width + RsFormInstance.Width, this.Location.Y);
+                TcpFormInstance.Init(ModuleIP.Text, Port.Text);
+            }
+            else
+            {
+                if( ModuleIP.Text.Length > 0 )
+                    ModuleIP.Text = "Enter IP";
+                if( Port.Text.Length > 0 )
+                    Port.Text = "Enter PORT";
+            }
+        }
+    }
+}
---
+++ EtherCOM/EtherCOM/TcpForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace EtherCOM
+{
+    partial class TcpForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TcpReceived = new System.Windows.Forms.TextBox();
+            this.TcpSend = new System.Windows.Forms.TextBox();
+            this.TcpReceivedLabel = new System.Windows.Forms.Label();
+            this.TcpSendLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TcpReceived
+            // 
+            this.TcpReceived.Location = new System.Drawing.Point(8, 60);
+            this.TcpReceived.Multiline = true;
+            this.TcpReceived.Name = "TcpReceived";
+            this.TcpReceived.Size = new System.Drawing.Size(270, 190);
+            this.TcpReceived.TabIndex = 11;
+            // 
+            // TcpSend
+            // 
+            this.TcpSend.Location = new System.Drawing.Point(48, 12);
+            this.TcpSend.Name = "TcpSend";
+            this.TcpSend.Size = new System.Drawing.Size(100, 20);
+            this.TcpSend.TabIndex = 10;
+            this.TcpSend.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TcpSend_OnKeyDown);
+            // 
+            // TcpReceivedLabel
+            // 
+            this.TcpReceivedLabel.AutoSize = true;
+            this.TcpReceivedLabel.Location = new System.Drawing.Point(7, 43);
+            this.TcpReceivedLabel.Name = "TcpReceivedLabel";
+            this.TcpReceivedLabel.Size = new System.Drawing.Size(107, 13);
+            this.TcpReceivedLabel.TabIndex = 9;
+            this.TcpReceivedLabel.Text = "Received/Send data";
+            // 
+            // TcpSendLabel
+            // 
+            this.TcpSendLabel.AutoSize = true;
+            this.TcpSendLabel.Location = new System.Drawing.Point(7, 15);
+            this.TcpSendLabel.Name = "TcpSendLabel";
+            this.TcpSendLabel.Size = new System.Drawing.Size(32, 13);
+            this.TcpSendLabel.TabIndex = 8;
+            this.TcpSendLabel.Text = "Send";
+            // 
+            // TcpForm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.TcpReceived);
+            this.Controls.Add(this.TcpSend);
+            this.Controls.Add(this.TcpReceivedLabel);
+            this.Controls.Add(this.TcpSendLabel);
+            this.Name = "TcpForm";
+            this.Text = "Communication over TCP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Send_label;
+        private System.Windows.Forms.TextBox Send;
+        private System.Windows.Forms.Label All_data;
+        private System.Windows.Forms.TextBox Data;
+        public System.Windows.Forms.TextBox TcpReceived;
+        private System.Windows.Forms.TextBox TcpSend;
+        private System.Windows.Forms.Label TcpReceivedLabel;
+        private System.Windows.Forms.Label TcpSendLabel;
+    }
+}
